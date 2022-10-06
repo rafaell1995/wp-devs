822af11ff31eac4bd7a7a1613b300835454d71d8
Rafael Araujo <r.araujo@jogajunto.com.br>
2022-10-05 21:19:25 -0300

    Create page layout static to split later

--- index.php
+++ index.php
@@ -7,5 +7,40 @@
 </head>
 <body <?php body_class(); ?>>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, incidunt neque! Recusandae quaerat ab in ullam quo dignissimos error voluptatem asperiores ducimus consequuntur repudiandae, deleniti, animi numquam molestias qui. Quam!</p>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <div id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </div>
+            </div>
+        </div>
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
 </body>
 </html>
