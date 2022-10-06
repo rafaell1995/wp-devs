ce2a86203393d0c0f9c67be47171a4e15344ad88
Rafael Araujo <r.araujo@jogajunto.com.br>
2022-10-05 21:09:35 -0300

    remove the title tag to be dynamically added later

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta <?php bloginfo( 'charset' ); ?>>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Devs</title>
 </head>
 <body>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, incidunt neque! Recusandae quaerat ab in ullam quo dignissimos error voluptatem asperiores ducimus consequuntur repudiandae, deleniti, animi numquam molestias qui. Quam!</p>
