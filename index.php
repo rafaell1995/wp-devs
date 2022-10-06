2dfdee8cc007d82e33cd5e6902ca978a3f95017b
Rafael Araujo <r.araujo@jogajunto.com.br>
2022-10-05 21:21:31 -0300

    Crop element to add header.php

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta <?php bloginfo( 'charset' ); ?>>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body <?php body_class(); ?>>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, incidunt neque! Recusandae quaerat ab in ullam quo dignissimos error voluptatem asperiores ducimus consequuntur repudiandae, deleniti, animi numquam molestias qui. Quam!</p>
-    <div id="page" class="site">
-        <header>
-            <section class="top-bar">
-                <div class="logo">
-                    Logo
-                </div>
-                <div class="searchbox">
-                    Search
-                </div>
-            </section>
-            <section class="menu-area">
-                <nav class="main-menu">
-                    Menu
-                </nav>
-            </section>
-        </header>
+
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <div id="main" class="site-main">
