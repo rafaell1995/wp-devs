a581a2ffedf7750644ded2008ad1609cc8f64201
Rafael Araujo <r.araujo@jogajunto.com.br>
2022-10-05 21:02:23 -0300

    Update index.php to test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Devs</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, incidunt neque! Recusandae quaerat ab in ullam quo dignissimos error voluptatem asperiores ducimus consequuntur repudiandae, deleniti, animi numquam molestias qui. Quam!</p>
+</body>
+</html>
