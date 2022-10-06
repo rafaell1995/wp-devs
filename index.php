eb871b7804614cf199c487d235a24aa2ccf84d8b
Rafael Araujo <r.araujo@jogajunto.com.br>
2022-10-05 21:11:51 -0300

    Add body_class() in body for add class dynamic

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body <?php body_class(); ?>>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, incidunt neque! Recusandae quaerat ab in ullam quo dignissimos error voluptatem asperiores ducimus consequuntur repudiandae, deleniti, animi numquam molestias qui. Quam!</p>
 </body>
 </html>
